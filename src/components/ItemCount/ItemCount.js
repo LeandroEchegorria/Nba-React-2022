7d6fa1b1bf66c4facae148aa39607a5577796ede
Leandro Echegorria <leandro.echegorria@gmail.com>
2022-03-26 19:42:44 -0300

    Corrections made in the itemCount component

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,31 +1,36 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import './ItemCount.css';
 
 
- const ItemCount = ({stock, onAdd}) => {
+ const ItemCount = ({stock}) => {
   const [count, setCount]= useState(1);
-  
+
   const onPlus = () => {
-    if (count < stock){
-      setCount(count + 1);
-  }}
+    (count < stock) && setCount(count + 1);
+  }
   const onRemove = () => {
-    if (count >0){
-      setCount(count - 1);
-  }}
+    (count >1) && setCount(count - 1)
+  }
   function onAdd () {
-      if (count >0){
-      console.log(`Se agregaron ${(count)} productos al carrito`)
-    }
-  } 
+    (count >0) && console.log(`Se agregaron ${(count)} productos al carrito`);  
+  }
+  useEffect( () => {
+    stock = stock-count;
+ 
+},onAdd)
   return (
-    <>
-      <button onClick={onRemove}>-</button>
-      <p>{count}</p>
-      <button onClick={onPlus}>+</button>
-      <p>Stock: {stock}</p>
-      <button className='buyBtn' onClick={onAdd}>Agregar</button>
-    </>
+    //Renderizado condicional
+    <div>
+    {(stock<1) ? <h2>Sin Stock</h2> 
+    : <div>
+        <button onClick={onRemove}>-</button>
+        <p>{count}</p>
+        <button onClick={onPlus}>+</button>
+        <p>Stock: {stock}</p>
+        <button className='buyBtn' onClick={onAdd}>Agregar</button>
+      </div>
+    }
+    </div>
   );
 }
 export default ItemCount;
--- src/components/ListProducts/ListProducts.js
+++ src/components/ListProducts/ListProducts.js
@@ -1,26 +1,91 @@
 import Card from '../Card/Card';
 import './ListProducts.css';
 import item1 from '../../../src/img/shoes/Adidas_Adizero_Rose1.png';
+import { useState, useEffect } from 'react';
 
 
 //componente declarado forma moderna
 const ListProducts = ({children}) => {
 
-    let dataProduct = {
-        title:'Adidas Adizero Rose 1',
-        size:9.5 ,
-        price:145,
-        image: item1,
-        stock: 5
+    const mockProductos =   [
+        {
+            id: 1,
+            title:'Adidas Adizero Rose 1',
+            size:9.5 ,
+            price:145,
+            image: item1,
+            stock: 0
+        },
+        {
+            id:2,
+            title:'Nike 1',
+            size: 8 ,
+            price: 145,
+            image: item1,
+            stock: 5
+        },
+        {
+            id: 3,
+            title:'Adidas Adizero Rose 2',
+            size:9.5 ,
+            price:145,
+            image: item1,
+            stock: 3
+        },
+        {
+            id: 4,
+            title:'Under Armour Curry 9',
+            size:9.5 ,
+            price:100,
+            image: item1,
+            stock: 0
+        },
+        {
+            id: 5,
+            title:'Adidas Trae Young 1',
+            size:9.5 ,
+            price:110,
+            image: item1,
+            stock: 2
+        },    
+        {
+            id: 6,
+            title:'Adidas Adizero Rose 3',
+            size:9.5 ,
+            price:125,
+            image: item1,
+            stock: 4
+        }
+    ]
+       
+    
+    const [products , setProducts]= useState([]);
+
+    const getProducts = () => {
+        return new Promise ( (resolve, reject) => {
+            return setTimeout ( () => {
+                resolve(mockProductos);
+            }, 3000);
+                
+        })
     }
+
+    useEffect( () => {
+        getProducts().then( (data) => {
+            setProducts(data)
+        }).finally( () => {console.log("Termino la llamada")})
+    }, )
+
     return (
         <>
-        <p className='title-product'>{children}</p>
+        <h2 className='title-product'> {children}</h2>
         <div className="container-cards">
-            <Card data={dataProduct}/>
-            <Card data={dataProduct}/>
-            <Card data={dataProduct}/>
-            <Card data={dataProduct}/>
+
+        {products.map( (producto , id) => {
+            return (
+                <Card data={producto} key={id}/>
+            )
+        })}
 
         </div>
         </>
